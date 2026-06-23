2800ff26b5aba97640bdbba8d96cb65ba9f75c04
Lee-LLiu <leeliupurpledon@gmail.com>
2026-06-23 11:21:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
 
+    const btn =
+        document.getElementById("compressBtn");
+
+    btn.addEventListener("click", ()=>{
+
+        document.getElementById("status")
+            .innerHTML =
+            "按钮工作正常";
+
+    });
+
+});
