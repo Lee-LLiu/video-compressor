582374873453c38e32e5a9c71858f0dd8c51a45f
Lee-LLiu <leeliupurpledon@gmail.com>
2026-06-23 03:55:39 +0000

    FFmpeg测试版V4

--- script.js
+++ script.js
@@ -3,85 +3,20 @@ let currentFile = null;
 const videoInput =
 document.getElementById("videoFile");
 
-videoInput.addEventListener("change", function(e){
+videoInput.addEventListener(
+"change",
+function(e){
 
-    const file = e.target.files[0];
-
-    if(!file) return;
-
-    currentFile = file;
-
-    const sizeMB =
-    (file.size / 1024 / 1024).toFixed(2);
-
-    const video =
-    document.createElement("video");
-
-    const url =
-    URL.createObjectURL(file);
-
-    video.preload = "metadata";
-
-    video.addEventListener(
-        "loadedmetadata",
-        function(){
-
-        const duration =
-        Math.floor(video.duration);
-
-        const minutes =
-        Math.floor(duration / 60);
-
-        const seconds =
-        duration % 60;
-
-        document.getElementById(
-            "info"
-        ).innerHTML =
-
-        `
-        <h3>视频信息</h3>
-
-        文件名：
-        ${file.name}
-
-        <br><br>
-
-        文件大小：
-        ${sizeMB} MB
-
-        <br><br>
-
-        时长：
-        ${minutes}分${seconds}秒
-
-        <br><br>
-
-        分辨率：
-        ${video.videoWidth}
-        ×
-        ${video.videoHeight}
-
-        <br><br>
-
-        预计压缩后：
-
-        ${(sizeMB * 0.3).toFixed(2)}
-        MB
-        （约减少70%）
-        `;
-
-        URL.revokeObjectURL(url);
-
-    });
-
-    video.src = url;
+    currentFile =
+    e.target.files[0];
 
 });
 
 document
 .getElementById("compressBtn")
-.addEventListener("click", function(){
+.addEventListener(
+"click",
+async function(){
 
     if(!currentFile){
 
@@ -90,10 +25,38 @@ document
         return;
     }
 
-    document.getElementById(
-        "status"
-    ).innerHTML =
+    const status =
+    document.getElementById("status");
 
-    "下一课开始接入FFmpeg.wasm";
+    status.innerHTML =
+    "开始加载FFmpeg...";
+
+    try{
+
+        const script =
+        document.createElement("script");
+
+        script.src =
+        "https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/umd/ffmpeg.js";
+
+        document.body.appendChild(
+        script
+        );
+
+        script.onload = function(){
+
+            status.innerHTML =
+            "FFmpeg加载成功";
+
+        };
+
+    }
+    catch(error){
+
+        status.innerHTML =
+        "加载失败：" +
+        error;
+
+    }
 
 });
