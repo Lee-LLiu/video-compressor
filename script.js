8b6ef9ee0085eab5c9d29cb587175f25273fc7c0
Lee-LLiu <leeliupurpledon@gmail.com>
2026-06-23 03:51:04 +0000

    视频分析版本V3

--- script.js
+++ script.js
@@ -1,14 +1,99 @@
-alert("V2版本已经加载");
+let currentFile = null;
 
-document.addEventListener("DOMContentLoaded", function () {
+const videoInput =
+document.getElementById("videoFile");
 
-    const btn = document.getElementById("compressBtn");
+videoInput.addEventListener("change", function(e){
 
-    btn.addEventListener("click", function () {
+    const file = e.target.files[0];
 
-        document.getElementById("status").innerHTML =
-            "这是新的 script.js 文件";
+    if(!file) return;
+
+    currentFile = file;
+
+    const sizeMB =
+    (file.size / 1024 / 1024).toFixed(2);
+
+    const video =
+    document.createElement("video");
+
+    const url =
+    URL.createObjectURL(file);
+
+    video.preload = "metadata";
+
+    video.addEventListener(
+        "loadedmetadata",
+        function(){
+
+        const duration =
+        Math.floor(video.duration);
+
+        const minutes =
+        Math.floor(duration / 60);
+
+        const seconds =
+        duration % 60;
+
+        document.getElementById(
+            "info"
+        ).innerHTML =
+
+        `
+        <h3>视频信息</h3>
+
+        文件名：
+        ${file.name}
+
+        <br><br>
+
+        文件大小：
+        ${sizeMB} MB
+
+        <br><br>
+
+        时长：
+        ${minutes}分${seconds}秒
+
+        <br><br>
+
+        分辨率：
+        ${video.videoWidth}
+        ×
+        ${video.videoHeight}
+
+        <br><br>
+
+        预计压缩后：
+
+        ${(sizeMB * 0.3).toFixed(2)}
+        MB
+        （约减少70%）
+        `;
+
+        URL.revokeObjectURL(url);
 
     });
+
+    video.src = url;
+
+});
+
+document
+.getElementById("compressBtn")
+.addEventListener("click", function(){
+
+    if(!currentFile){
+
+        alert("请先选择视频");
+
+        return;
+    }
+
+    document.getElementById(
+        "status"
+    ).innerHTML =
+
+    "下一课开始接入FFmpeg.wasm";
 
 });
